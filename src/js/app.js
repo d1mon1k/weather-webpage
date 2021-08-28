cab9b9ec2b7915d37057bcfbfa5fa46760956283
D1mon1k <ostrovskiy.a.97@gmail.com>
2021-08-28 16:02:26 +0300

    feat: add hours container on click on the city

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,15 @@
 //TODO add preloader
-//TODO add weather for the next day
+//TODO add block for the weather on the next day
 //TODO add beautiful animations
 
-const defaultCities = ['Moscow', 'London', 'Tokio', 'Pekin', 'New York', 'Paris'];
+const defaultCities = ['Moscow', 'London', 'Tokyo', 'Pekin', 'New York', 'Paris'];
 const apiUrl = 'http://api.weatherapi.com/v1/forecast.json?'
 const apiKey = 'key=5050ce131d7f4d3ab69140830211708'
-// const path = '&q=Minsk&days=7'
 
 const mainContainer = document.querySelector('#main-container')
-const listContainer = document.querySelector('#list-container')
-const form = document.forms.submitCity
+const citiesContainer = document.querySelector('#cities-container')
+const hoursContainer = document.querySelector('#hours-container')
+// const form = document.forms.submitCity
 
 async function renderCitiesWeather(citiesArr) {
   return citiesArr.forEach((city) => {
@@ -20,8 +20,8 @@ async function renderCitiesWeather(citiesArr) {
 async function renderWeatherCard(path, url = apiUrl, key = apiKey) {
   const data = await getWeatherJson(path, url, key)
   const listItem = weatherCardTemplate(data)
-  listContainer.className = 'cards-container'
-  listContainer.append(listItem)
+  citiesContainer.className = 'cards-container'
+  citiesContainer.append(listItem)
 }
 
 async function getWeatherJson(path, url = apiUrl, key = apiKey) {
@@ -31,52 +31,110 @@ async function getWeatherJson(path, url = apiUrl, key = apiKey) {
 }
 
 function weatherCardTemplate(data) {
-  let {
+  const {
     location: { name: city },
     current: { temp_c: temperature },
     current: { condition: { icon } }
   } = data
 
   const itemBody = `<h3 class="card__title">${city}</h3>
-                    <div class="card__temperature">${temperature}&deg</div>
+                    <div class="card__temperature">${getWholeNum(temperature)}&deg</div>
                     <div class="card__img"><img src="${icon}"></div>`
   const listItem = document.createElement('li')
   listItem.classList.add('card')
+  listItem.dataset.city = city
   listItem.insertAdjacentHTML('afterbegin', itemBody)
   listItem.addEventListener('click', onClickHandler)
   return listItem
 }
 
-// function renderWeek(path, url = apiUrl, key = apiKey) {
-//   const data = getWeatherJson(path)
-//   container.innerHTML = ''
-//   container.classList.remove('cards-container')
-//   container.classList.add('cards-container_weekday')
-//   return container.insertAdjacentHTML('afterbegin', weekdayCardTemplate(data))
-// }
-
-// function weekdayCardTemplate(data) {
-//   return `<li class="card-weekday">
-//             <div class="card-weekday__title">Monday</div>
-//             <div class="card-weekday__image"></div>
-//             <div class="card-weekday__temperature">
-//               <span>22&deg</span>
-//               <span>13&deg</span>
-//             </div>
-//           </li> `
-// }
-
-
-// renderCitiesWeather(defaultCities)
-
 function onClickHandler(e) {
-  // renderWeek(`&q=${this.firstChild.textContent}&days=3`)
-  console.log("onClickHandler")
+  renderHoursSection(`&q=${this.dataset.city}&days=3`)
 }
-// container.addEventListener('click', onClickHandler)
+
+async function renderHoursSection(path, url = apiUrl, key = apiKey) {
+  const data = await getWeatherJson(path)
+  const {
+    forecast: { forecastday: [day0] }
+  } = data
+  const { hour: hours } = day0
+
+  citiesContainer.innerHTML = ''
+
+  mainContainer.insertAdjacentHTML('afterbegin', cardDetailsTemplate(data))
+  for (let hour of hours) {
+    hoursContainer.insertAdjacentHTML('afterbegin', hoursCardTemplate(hour))
+  }
+}
+
+function cardDetailsTemplate(data) {
+  const {
+    location: { name: city },
+    location: { country },
+    forecast: { forecastday: [day0] }
+  } = data
+  const {
+    date,
+    day: { maxtemp_c },
+    day: { mintemp_c },
+    day: { daily_chance_of_rain }
+  } = day0
+
+  return `<div class="card-details">
+          <table class="card-details__table">
+            <tr>
+              <td class="card-details__info">Country:</td>
+              <td class="card-details__text">${country}</td>
+            </tr>
+            <tr>
+              <td class="card-details__info">City:</td>
+              <td class="card-details__text">${city}</td>
+            </tr>
+            <tr>
+              <td class="card-details__info">Date:</td>
+              <td class="card-details__text">${date}</td>
+            </tr>
+            <tr>
+              <td class="card-details__info">Temperature:</td>
+              <td class="card-details__text">${getWholeNum(maxtemp_c)}&deg<span>${getWholeNum(mintemp_c)}&deg</span></td>
+            </tr>
+            <tr>
+              <td class="card-details__info">Daily chance of rain:</td>
+              <td class="card-details__text">${daily_chance_of_rain}%</td >
+            </tr >
+          </table >
+        </div > `
+}
+
+function hoursCardTemplate(data) {
+  const {
+    time,
+    condition: { icon },
+    temp_c: temperature,
+  } = data
+
+  return `<li class="hours-list__item" >
+              <time class="hours-list__time">${time.substr(-5)}</time>
+              <div class="hours-list__image" style="background-image: url('${icon}')"></div>
+              <div class="hours-list__temperature">${getWholeNum(temperature)}&deg</div>
+            </li> `
+}
+
+function getWholeNum(digit) {
+  return Math.round(Number(digit))
+}
+
+renderCitiesWeather(defaultCities)
 
 
 
+
+
+
+
+
+
+//====================================================================
 // let obj = {
 //   name: 'Artyom',
 //   foo: {
@@ -90,8 +148,6 @@ function onClickHandler(e) {
 
 // let { foo: { bar: { fus } }, foo } = obj
 // fus //?
-
-
 
 
 
